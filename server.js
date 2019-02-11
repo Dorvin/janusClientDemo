3598dc7c612967bfcd9baab5b9faf9e5bf6f575b
Dorvin <ladig1017@gmail.com>
2019-02-11 16:26:29 +0900

    initial comit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('demo'));
+
+app.get('/', function (req, res) {
+  res.send('go to /screensharingtest.html')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
